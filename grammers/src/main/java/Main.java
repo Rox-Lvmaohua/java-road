492a643f14db8c387f1165390451a94698003bdd
maohua lv <1093656358@qq.com>
2024-04-19 19:54:24 +0800

    init project

---
+++ grammers/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author :
+ * @Date: 2024/4/19 19:52
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
