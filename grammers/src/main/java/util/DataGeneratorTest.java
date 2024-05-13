5bce0047e187c6f6178da0fdaea13360de689f55
lvmaohua <1093656358@qq.com>
2024-05-13 20:53:47 +0800

    添加函数式接口数据生成工具类，spock依赖

---
+++ grammers/src/main/java/util/DataGeneratorTest.java
@@ -0,0 +1,9 @@
+package util;
+
+public class DataGeneratorTest {
+    public static void main(String[] args) {
+        System.out.println("DataGeneratorUtil.generateMap(5) = " + DataGeneratorUtil.generateMap(5));
+        System.out.println("DataGeneratorUtil.generateSet(5) = " + DataGeneratorUtil.generateSet(5));
+        System.out.println("DataGeneratorUtil.generateList(5) = " + DataGeneratorUtil.generateList(5));
+    }
+}
---
+++ grammers/src/main/java/util/DataGeneratorUtil.java
@@ -0,0 +1,33 @@
+package util;
+
+import com.github.javafaker.Faker;
+
+import java.util.*;
+
+public class DataGeneratorUtil {
+    private static final Faker faker = new Faker();
+
+    public static <K, V> Map<K, V> generateMap(int size) {
+        Map<K, V> map = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            map.put((K) faker.lorem().word(), (V) faker.lorem().sentence());
+        }
+        return map;
+    }
+
+    public static <T> Set<T> generateSet(int size) {
+        Set<T> set = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add((T) faker.lorem().word());
+        }
+        return set;
+    }
+
+    public static <T> List<T> generateList(int size) {
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add((T) faker.lorem().word());
+        }
+        return list;
+    }
+}
